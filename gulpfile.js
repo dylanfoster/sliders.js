0de65db6c848ec81d04d6e8885ad1aaf2c543528
Dylan Foster <dylan_foster@apple.com>
2014-10-25 17:06:34 -0700

    changes build structure and adds gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var gulp = require("gulp");
+var eslint = require("gulp-eslint");
+var jsonlint = require("gulp-jsonlint");
+var uglify = require("gulp-uglify");
+var minifyCSS = require("gulp-minify-css");
+var rename = require("gulp-rename");
+
+var css = ["./src/**/*.css"];
+var js = ["./src/**/*.js"];
+
+gulp.task("default", ["lint", "build"]);
+
+gulp.task("build", function(){
+  gulp.src(css)
+  .pipe(minifyCSS())
+  .pipe(rename(function(file){
+    file.basename += ".min";
+    file.ext =".css";
+  }))
+  .pipe(gulp.dest("./build"));
+
+  gulp.src(js)
+  .pipe(uglify())
+  .pipe(rename(function(file){
+    file.basename += ".min";
+    file.ext = ".js";
+  }))
+  .pipe(gulp.dest("./build"));
+});
